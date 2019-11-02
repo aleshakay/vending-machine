345f2b1cc7c27c2fbaa607be02c1df69c70230bc
alesha reed <aleshak83@gmail.com>
2019-11-02 10:33:14 -0500

    add db

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,18 @@
-import 'bootstrap';
 import firebase from 'firebase';
-import apiKeys from './helpers/apiKeys';
+
+import auth from './components/auth/auth';
+import authData from './helpers/authData';
+import myNavbar from './components/myNavbar/myNavbar';
+
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 
 const init = () => {
-  firebase.initializeApp(apiKeys, 'firebaseKeys');
-  console.error('hi', apiKeys, 'firebaseKeys');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
