cc24ef7dd487402418cb5f9701f323dbd8c00b9d
alesha reed <aleshak83@gmail.com>
2019-11-09 00:19:20 -0600

    update code to latest

--- src/javascripts/components/stocker/stocker.js
+++ src/javascripts/components/stocker/stocker.js
@@ -1,14 +1,17 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+
 import smash from '../../helpers/data/smash';
-import utilities from '../../helpers/utilities';
 import stockCard from '../stockCard/stockCard';
-import './stocker.scss';
+import machine from '../Machine/machine';
+
 import snackPositionData from '../../helpers/data/snackPositionData';
 import snackData from '../../helpers/data/snackData';
 
-import machine from '../Machine/machine';
+import utilities from '../../helpers/utilities';
+
+import './stocker.scss';
 
 const deleteFromMachine = (e) => {
   e.preventDefault();
@@ -66,6 +69,19 @@ const addNewSnack = (e) => {
     .catch((error) => console.error(error));
 };
 
+const quickStock = (e) => {
+  e.stopImmediatePropagation();
+  const snackId = e.target.id.split('snack-')[1];
+  snackData.restock(snackId, 5)
+    .then(() => {
+      const { uid } = firebase.auth().currentUser;
+      // eslint-disable-next-line no-use-before-define
+      buildTheStocker(uid);
+      machine.buildTheMachine();
+    })
+    .catch((error) => console.error(error));
+};
+
 const buildTheStocker = (uid) => {
   smash.getSnacksWithPositions(uid)
     .then((snacks) => {
@@ -81,6 +97,7 @@ const buildTheStocker = (uid) => {
       utilities.printToDom('stock', domString);
       $('#stock').on('click', '.delete-snack-position', deleteFromMachine);
       $('#stock').on('click', '.add-snack-position', addToMachine);
+      $('#stock').on('click', '.quick-stock', quickStock);
       $('#add-new-snack').click(addNewSnack);
     })
     .catch((error) => console.error(error));
